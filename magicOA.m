f361d709d80be51cad596f1ab31c1243cf84c46b
kilir <ruediger.kilian@protonmail.com>
2021-12-15 16:01:14 +0100

    initial commit

---
+++ magicOA.m
@@ -0,0 +1,39 @@
+function T = magicOA(varargin)
+% calculate the super precise temprature from a quartz c-axis opening
+% angle, error of the fit is about 50 degree
+% based on Faleiros et al.,2016
+%
+% Usage:
+% case1: opening angle (in degree) and P (in kBar) is known
+% T = magicOA(OA,P)
+% 
+% case 2: opening angle (in degree) is known
+% T = magicOA(OA)
+%
+% case 3: neither opening angle nor pressure is known
+% T = magicOA
+%
+% Note: the uncertainty that comes with case 3 might be high and the result
+% might be unreliable, but anyhow it might be worth giving it a try
+
+if length(varargin) ==2
+    OA =  mod(varargin{1},180);
+    P =  varargin{2};
+    T= 410.44*log(OA)+14.22*P-1272;
+end
+
+if length(varargin) ==1
+    OA =  mod(varargin{1},180);
+    if OA<=87
+        T=6.9*OA+48;
+        if T<250
+            disp('Too cold, T-range not defined, please make up another opening angle');
+        end
+    else 
+        T=4.6*OA+258;
+    end
+end
+
+if isempty(varargin)
+    T = 250 + 800.*rand(1,1);
+end
